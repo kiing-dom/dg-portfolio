7e58a10bf9b0b3cd5adb1ff129a719f13ae93ba2
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-07-16 18:25:27 +0100

    removed errors causing conflicts with vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import { Worker } from "@react-pdf-viewer/core";
 
 const inter = Inter({ subsets: ["latin"] });
 
