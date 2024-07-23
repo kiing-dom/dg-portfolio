97dad3c99be198482da61e453738a4e6ab90fb6c
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-07-23 08:07:36 +0100

    added animations from framer-motion to hero and experiences | added headstarter fellowship to experiences

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -8,6 +8,7 @@ import { Button } from '../ui/button';
 import Image from 'next/image';
 import { Montserrat, Noto_Sans, Lato } from 'next/font/google';
 import Typewriter from 'typewriter-effect/dist/core';
+import { motion } from 'framer-motion';
 
 const noto = Noto_Sans({
   subsets: ["latin"],
@@ -77,24 +78,46 @@ const Hero: React.FC = () => {
 
   return (
     <div className='relative h-screen'>
-      <Starfield
-        starCount={1000}
-        starColor={[134, 134, 189]}
-        speedFactor={0.15}
-      />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        className='absolute inset-0'
+      >
+        <Starfield
+          starCount={1000}
+          starColor={[134, 134, 189]}
+          speedFactor={0.15}
+        />
+      </motion.div>
       <div className='absolute inset-0 bg-black opacity-50'></div>
       <div className='relative z-10 flex flex-col items-center justify-center h-full text-white px-4'>
-        <div className="flex flex-col md:flex-row items-center">
-          <div className="hidden md:block lg:block order-1 md:order-2 mt-8 md:mt-0 md:ml-8 w-52 h-52 md:w-72 md:h-72">
+        <motion.div
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 3 }}
+          className="flex flex-col md:flex-row items-center"
+        >
+          <motion.div
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 3.3 }}
+            className="hidden md:block lg:block order-1 md:order-2 mt-8 md:mt-0 md:ml-8 w-52 h-52 md:w-72 md:h-72"
+          >
             <Image
               src="/assets/images/personal-photo.jpg"
               width={300}
               height={300}
-              alt="portfolio main image"
+              alt="dominion gbadamosi portfolio main image"
               className='object-none w-48 h-48 md:w-72 md:h-72 rounded-full custom-position hover:scale-125 transition-transform'
             />
-          </div>
-          <div className='order-2 md:order-1 flex flex-col items-center text-center md:text-left'>
+          </motion.div>
+          <motion.div
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 3.8 }}
+            className='order-2 md:order-1 flex flex-col items-center text-center md:text-left'
+          >
             <div style={noto.style} className='text-4xl md:text-7xl font-bold typewriter'></div>
             <h1 style={noto.style} className="text-4xl md:text-7xl font-bold">Gbadamosi</h1>
             <div className='w-[70%]'>
@@ -131,8 +154,8 @@ const Hero: React.FC = () => {
                 </div>
               </div>
             )}
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
     </div>
   );
